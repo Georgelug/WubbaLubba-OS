ef7b85c0d9b893577b5663918487247d13a0d610
Georgeluguuser] <esparzajorge03@gmail.com>
2021-07-19 02:40:55 -0500

    Se agrego una despedida

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ int main(){
     
     // funcion menu, la cual abre el menu principal
     // menu();
+    
     cout << "\n\n\tHola mundo, esto es jakass"<<endl;
     
     Circulo c(5);
     c.showData();
+
+    cout << "\n\n\tsale bye!"<<endl;
 
     return 0;
 }
